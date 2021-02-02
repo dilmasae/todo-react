6f4d3d451bfddfb8e76c93305e76bcb2fde66724
dilma sae <dilmasae@gmail.com>
2021-02-02 22:48:31 +0100

    added bootstrap and sass

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Todo from './components/todo/Todo';
 import reportWebVitals from './reportWebVitals';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/sass/styles.scss";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Todo /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
